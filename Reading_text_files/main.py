2a668ba10a7cbac9f77dc7eeb72494bd30e71af1
unknown <tobiloba110@gmail.com>
2022-06-08 15:35:18 +0100

    reading text files

--- Reading_text_files/main.py
+++ Reading_text_files/main.py
@@ -1,4 +1,4 @@
-# Read text from a file, and count the occurence of words in that text
+# Read text from a file, and count the occurrence of words in that text
 # Example:
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
@@ -32,6 +32,5 @@ def count_words():
 
 
 print(count_words())
-
 
 
